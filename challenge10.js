e163d190cd75cb4fffbdbaadad7eec6839ca8869
ashleyromano <ashleye.romano@gmail.com>
2022-01-12 18:39:45 -0500

    initial thoughts

---
+++ challenge10.js
@@ -0,0 +1,57 @@
+// Power of Two
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+// Examples
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+
+// Input: n = 16
+// Output: true
+// Explanation: 24 = 16
+
+// Input: n = 3
+// Output: false
+
+
+// Extra Challenge
+// Could you solve it without loops/recursion?
+
+// Constraints
+// -231 <= n <= 231 - 1
+
+//                _______________________________________________________________
+//
+// notes
+//
+// 2 ^ n
+// n = integer
+// n must = 2 to the somethingth power
+// or
+// 2 * 2 * 2 * 2 ....
+// so
+// create function that asks ?
+// does 2 to the somethingth power = n ?
+// or 
+// if n is divided by 2 repeatedly will it reduce down to 2 at some point(the number of repeats would be the exponent of 2 in each case)(eventually down to 0) ?
+// / 
+// will it have no remainder(but needs to be more specific than that)--keep thinking on this ?
+
+//     function powerOfTwo(n) {
+//         figure out how to check if power of 2 as above
+//         if/else w boolean?
+//         what about returning the exponent value too, just bc?
+// }
+
+//  + what the hell is this ?
+// Constraints
+// -231 <= n <= 231 - 1
+//                  _______________________________________________________________
+
+
+function powerOfTwo(n) {
+
+    
+};
+    
