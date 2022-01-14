745d9eaa53dd320d7b6ce4f4c153b54a277e98ca
ashleyromano <ashleye.romano@gmail.com>
2022-01-14 17:20:14 -0500

    added text

---
+++ challenge12.js
@@ -0,0 +1,14 @@
+Max Sub Array Sum 
+Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array
+
+Examples
+maxSubarraySum([1,2,5,2,8,1,5] , 2) // 10
+maxSubarraySum([1,2,5,2,8,1,5] , 4) // 17
+maxSubarraySum([4,2,1,6] , 1) // 6
+maxSubarraySum([] , 4) // null
+
+Extra Challenge
+Solve in O(N)
+
+Constraints
+N/A
